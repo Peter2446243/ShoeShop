d20581891ba9644267f8a896015b9e98193b23a4
B1agile@outlook.com <B1agile@outlook.com>
2020-02-13 10:42:49 +0000

    Added testing for supplier

---
+++ Shoe Testing/tstSupplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shoe_Testing
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void InstanceOK2()
+        {
+            clsSupplier aSupplier = new clsSupplier();
+            Assert.IsNotNull(aSupplier);
+        }
+    }
+}
