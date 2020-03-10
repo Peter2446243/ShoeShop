877bbd847f26180faba1e7a08ecd1f2a0e1059f6
cbbell98@gmail.com <cbbell98@gmail.com>
2020-03-10 12:04:30 +0000

    Added testing and Web form for Staff Management

--- Shoe Testing/tstStaff.cs
+++ Shoe Testing/tstStaff.cs
@@ -15,5 +15,149 @@ namespace Shoe_Testing
             //test to see that it exists
             Assert.IsNotNull(AStaff);
         }
+
+        [TestMethod]
+        public void StaffIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStaff.StaffID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.StaffID, TestData);
+        }
+
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Sales";
+            //assign the data to the property
+            AStaff.Department = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Department, TestData);
+        }
+
+        [TestMethod]
+        public void JobTitlePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Sales Manager";
+            //assign the data to the property
+            AStaff.JobTitle = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.JobTitle, TestData);
+        }
+
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Joe Bloggs";
+            //assign the data to the property
+            AStaff.Name = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Name, TestData);
+        }
+
+        [TestMethod]
+        public void AvailablePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AStaff.Available = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Available, TestData);
+        }
+
+        [TestMethod]
+        public void ExtensionPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "123";
+            //assign the data to the property
+            AStaff.Extension = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Extension, TestData);
+        }
+
+        [TestMethod]
+        public void PhoneNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "07123456789";
+            //assign the data to the property
+            AStaff.PhoneNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.PhoneNo, TestData);
+        }
+
+        [TestMethod]
+        public void HomeAddressPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "21b Some Street";
+            //assign the data to the property
+            AStaff.HomeAddress = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.HomeAddress, TestData);
+        }
+
+        [TestMethod]
+        public void PostcodePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "LE1 2AB";
+            //assign the data to the property
+            AStaff.Postcode = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Postcode, TestData);
+        }
+
+        [TestMethod]
+        public void CityPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Leicester";
+            //assign the data to the property
+            AStaff.City = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.City, TestData);
+        }
+
+        [TestMethod]
+        public void EnrolmentDatePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStaff.EnrolmentDate = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.EnrolmentDate, TestData);
+        }
+
     }
 }
---
+++ ShoeFrontOffice/AStaff.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AStaff.aspx.cs" Inherits="AStaff" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        <p>
+          <label>Staff ID</label>
+          <input type = "text"
+                 id = "txtStaffID"
+                 value = "" />
+        </p>
+        <p>
+          <label>Department</label>
+          <input type = "text"
+                 id = "txtDepartment"
+                 value = "" />
+        </p>
+            <p>
+            <label>Job Title</label>
+          <input type = "text"
+                 id = "txtJobTitle"
+                 value = "" />
+         </p>
+            <p>
+            <label>Name</label>
+          <input type = "text"
+                 id = "txtName"
+                 value = "" />
+         </p>
+            <p>
+            <label>Extension</label>
+          <input type = "text"
+                 id = "txtExtension"
+                 value = "" />
+         </p>
+            <p>
+            <label>Phone No</label>
+          <input type = "text"
+                 id = "txtPhoneNo"
+                 value = "" />
+         </p>
+            <p>
+            <label>Home Address</label>
+          <input type = "text"
+                 id = "txtHomeAddress"
+                 value = "" />
+         </p>
+            <p>
+            <label>PostCode</label>
+          <input type = "text"
+                 id = "txtPostCode"
+                 value = "" />
+         </p>
+            <p>
+            <label>City</label>
+          <input type = "text"
+                 id = "txtCity"
+                 value = "" />
+         </p>
+            <p>
+            <label>Enrolment Date</label>
+          <input type = "text"
+                 id = "txtEnrolmentDate"
+                 value = "" />
+         </p>
+            <p>
+            <label>Available</label>
+          <input type = "checkbox"
+                 id = "chkAvailable"
+                 value = "" />
+         </p>
+            <p>
+                <button type = "Submit" onclick="OK" value= "OK"> OK </button>
+               <button type = "reset" value = "Cancel"> Cancel </button>
+         </p>
+        </div>
+    </form>
+</body>
+</html>
